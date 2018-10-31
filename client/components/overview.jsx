c433bcc5daea34b3b17585a3aa4970b57abb623c
Steven Chung <stevenchung.213@gmail.com>
2018-10-31 15:54:32 -0700

    Add files via upload

--- client/components/overview.jsx
+++ client/components/overview.jsx
@@ -1,19 +1,81 @@
 import React from 'react';
+import $ from 'jquery';
+
+
 
 export default class Overview extends React.Component {
-  constructor (props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-
+      init: false,
+      properties: null
     };
+
+  }
+
+  componentDidMount() {
+    $.get('/listings', result => {
+      console.log('success ', result)
+      this.setState({properties: result, init: true})
+    }, 'json');
+
   }
 
   render() {
     return (
-      <React.Fragment>
-        <div>hello world</div>
-      </React.Fragment>
+      <div>
+        {this.state.init &&
+        (<div id="main-container">
+          <div id="nav-bar">
+            <ul className="nav-bar-list">
+              <li className="active"><a href="#overview">Overview</a></li>
+              <li className=""><a href="#amenities">Amenities</a></li>
+              <li className=""><a href="#reviews">Reviews</a></li>
+              <li className=""><a href="#map">Map</a></li>
+              <li className=""><a href="#availability">Availability</a></li>
+            </ul>
+
+            <div>
+              <span id="minimap"></span>
+
+              <h2 id="headline">{this.state.properties[0].headline}</h2>
+            </div>
+            <section className="icon-bar">
+              <div id="icons">
+                <ul>
+                  <li id="type">
+                    <span>{this.state.properties[0].type}</span>
+                    <span>{this.state.properties[0].area}</span>
+                  </li>
+                  <li id="bedrooms">
+                    <span>Bedrooms</span>
+                    <span>{this.state.properties[0].bedrooms}</span></li>
+                  <li id="sleeps">
+                    <span>Sleeps</span>
+                    <span>{this.state.properties[0].sleeps}</span>
+                  </li>
+                  <li id="bathrooms">
+                    <span>Bathrooms</span>
+                    <span>{this.state.properties[0].bathrooms}</span>
+                  </li>
+                  <li id="halfbaths">
+                    <span>Half Baths</span>
+                    <span>{this.state.properties[0].halfBaths}</span>
+                  </li>
+                  <li id="minstay">
+                    <span>Min Stay</span>
+                    <span>{this.state.properties[0].minStay}</span>
+                  </li>
+                </ul>
+              </div>
+            </section>
+
+          </div>
+        </div>)
+        }
+      </div>
+
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,30 +1,59 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {overview} = require('../db/index.js');
+const path = require('path');
 
-let app = express();
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/../public'));
-
+app.use(express.static(path.resolve(__dirname + '/../public')));
 
 app.get('/', (req, res) => {
-  res.redirect('index.html');
+  res.redirect('index.html')
+});
+
+app.get('/listings', (req, res) => {
+  let getDbData = (callback) => {
+    overview.find({}, (err, docs) => {
+      if (err) {
+        console.log('ERROR ERROR ERROR')
+        callback(err, null);
+      }
+      console.log('GET REQUEST SERVED')
+      callback(docs);
+    });
+  };
+  getDbData(result => {
+    console.log('sending db results')
+    res.status(200).sendFile('index.html', {root: './public'});
+  })
+});
+
+app.get('/listings/:propertyID', (req, res) => {
+  console.log(req.params)
+  let propID = req.params.propertyID;
+  overview.find({propertyID: propID}).exec()
+    .then(result => res.status(200).send(result))
+    .catch(err => console.log('ERROR \n', err));
+  // let getSpecificListing = (callback) => {
+  //   overview.find({ propertyID: propID }, (err, doc) => {
+  //     if (err) {
+  //       console.log('ERROR @ SPECIFIC LISTING', err)
+  //       callback(err, null)
+  //     }
+  //     console.log('GET @ SPECIFIC LISTING SERVED')
+  //     callback(doc)
+  //   });
+  // };
+  // getSpecificListing(result => {
+  //   console.log('sending specific db result')
+  //   res.sendfile('index.html', result);
+  // })
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-let port = 3000;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
