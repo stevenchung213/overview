225b2be56095ce3d8d2c843dfea3ebc66e8110f8
Steven Chung <stevenchung.213@gmail.com>
2018-10-31 22:11:30 -0700

    api calls + tests for db

--- server/index.js
+++ server/index.js
@@ -10,9 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname + '/../public')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/../public/index.html'));
-// });
 
 app.get('/listings', (req, res) => {
   let getDbData = (callback) => {
@@ -40,25 +37,6 @@ app.get('/listings/:propertyID', (req, res) => {
     .then(result => res.status(200).send(result))
     .catch(err => console.log('ERROR \n', err));
 });
-
-
-
-  // let getSpecificListing = (callback) => {
-  //   overview.find({ propertyID: propID }, (err, doc) => {
-  //     if (err) {
-  //       console.log('ERROR @ SPECIFIC LISTING', err)
-  //       callback(err, null)
-  //     }
-  //     console.log('GET @ SPECIFIC LISTING SERVED')
-  //     callback(doc)
-  //   });
-  // };
-  // getSpecificListing(result => {
-  //   console.log('sending specific db result')
-  //   res.sendfile('index.html', result);
-
-
-
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
