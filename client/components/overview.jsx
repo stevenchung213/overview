e4b0c5c0717106743b88b956b48c147152fcbf38
Steven Chung <stevenchung.213@gmail.com>
2018-11-08 18:10:54 -0800

    cors dependency added

--- client/components/overview.jsx
+++ client/components/overview.jsx
@@ -6,8 +6,6 @@ import Icons from './Icons.jsx';
 import Description from './Description.jsx';
 
 
-const port = 8081;
-
 export default class Overview extends React.Component {
   constructor() {
     super();
--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {overview} = require('../db/index.js');
 const path = require('path');
-// const normalizePort = require('normalize-port');
+const cors = require('cors');
 
 const app = express();
-// const port = 3001;
+
 const port = 8081;
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname + '/../public')));
